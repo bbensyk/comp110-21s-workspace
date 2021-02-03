32da7144331683037af0bfa1defcca0e58ef471b
Ben Sykes <bben@email.unc.edu>
2021-02-03 18:26:16 -0500

    Added the fortune cookie program.

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730442945"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,35 @@ from random import randint
 
 
 # Begin your solution here...
+a: int = int(randint(1,10))
+
+print("Your fortune cookie says... ")
+
+if a < 5:
+    if a == 1:
+        print("Does this seem real?")
+    else:
+        if a ==2:
+         print("Look Behind you.")
+        if a == 3:
+         print("Are you real?")
+        if a == 4:
+            print("Don't trust anything.")
+else: 
+    if a == 5:
+        print("Do you feel like you're being watched.")
+    else:
+        if a < 8: 
+            if a == 6:
+                print("I'll see you later.")
+            else:
+                print("NO ")
+        if a < 10:
+            if a == 8:
+                print: "I am ready, are you?"
+            if a == 9:
+             print("666")
+        else:
+            print("Hi :)")
+    
+print("Now, go spread possitive vibes.")
---
+++ lessons/input.py
@@ -0,0 +1,6 @@
+"""A Program to demonstrate user input and variables."""
+
+name: str = input("Who are you? ")
+
+print("Wow, "+ name + ",you rock!")
+print(name+ " have a great day!")
