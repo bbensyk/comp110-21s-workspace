cec87b498c7c17df8d03e7463cfd5f78d12d41a6
Ben Sykes <bben@email.unc.edu>
2021-02-11 12:38:05 -0500

    ex2

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -2,17 +2,30 @@
 
 from random import randint
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730442945"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
     # TODO 2: Print the result of calling your fortune_cookie function.
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
 
 
 # TODO 1: Define your fortune_cookie function here.
+def fortune_cookie() -> str:
+    fortune: int = randint(1,4)
+    if fortune <3:
+        if fortune == 1: 
+            return("Man who runs behind car gets exhausted.")
+        else:
+            return("Man who runs infront of car gets tired.")
+    else:
+        if fortune == 4:
+            return("Eat more fortune cookies.")
+        else:
+            return("Go to gym, delete facebook.")
 
 
 # Python Idiom for "starting" the program when run as a module.
--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -7,10 +7,20 @@ def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
     # TODO 2: Print the response of calling the tar_heels function here.
+    print(tar_heels(choice))
 
 
 # TODO 1: Define the tar_heels function, and its logic, here.
-
+def tar_heels(choice:int) -> str: 
+    if (choice % 2 == 0 )and (choice % 7== 0):
+        return("TAR HEELS")
+    else:
+        if choice % 2 ==0:
+            return("TAR")
+        if choice % 7 == 0:
+            return("HEELS")
+        else:
+            return("CAROLINA")
 
 if __name__ == "__main__":
     main()
